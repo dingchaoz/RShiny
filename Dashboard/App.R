64c1cf70b323937b42c91b9bd3815379175c496b
Sri Seshadri <ku906@cummins.com>
2015-12-19 16:36:47 -0500

    bug fixes to sclaing of dot/box plot by trucks

--- Dashboard/App.R
+++ Dashboard/App.R
@@ -231,15 +231,15 @@ server <- function(input,output,session){
                 
                 
                 if (nrow(Data) > 0 ){
-                        
+
                         output$Tplot <- renderPlot({
                                 
                                 
                                 # TgtDat <- Data$TruckName
                                
-                                browser()
+                               
                                 p <-ggplot(data = Data,aes(x=TruckName,y=Val,color = TruckName))+geom_boxplot(outlier.colour = "white")+  geom_jitter(position = position_jitter(0.1,0)) +  coord_flip()+ theme_bw()+ theme(legend.position = "none") + theme(axis.title.y = element_blank())+ ylab(paste(Parameter,"\n",paste("PpK =",DescSats$PpK,"Mean =", DescSats$Average, "Std.dev =",DescSats$Stdev, "Failures:",DescSats$Failures)))
-                                p <- p + geom_hline(yintercept = c(USL_Value),color = "Red", linetype = "longdash" ) + ggtitle(bquote(atop(.(input$Diag), atop(italic(.(input$Program)),atop(.(input$TrucksGrp), ""))))) +  scale_y_continuous(limits = c(min(c(Data$Val,LSL_Value - 0.5),na.rm = T),max(c(Data$Val,USL_Value),na.rm = T)),breaks = scales::pretty_breaks(n = 10))
+                                p <- p + geom_hline(yintercept = c(LSL_Value,USL_Value),color = "Red", linetype = "longdash" ) + ggtitle(bquote(atop(.(input$Diag), atop(italic(.(input$Program)),atop(.(input$TrucksGrp), ""))))) +  scale_y_continuous(limits = c(min(c(Data$Val,LSL_Value - 0.5,USL_Value-0.5),na.rm = T),max(c(Data$Val,USL_Value +0.5, LSL_Value + 0.5),na.rm = T)),breaks = scales::pretty_breaks(n = 10))
                                 if(!is.na(LSL_Value)){
                                 p <- p + geom_text(data = NULL,y = LSL_Value,x = 0.5, label = "LSL", color = "red")
                                 }
